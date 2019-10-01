cac6baccb06c897e447433542caa44bb8a332696
Timothy Hood <tjhood97@gmail.com>
2019-09-30 20:33:33 -0400

    Created new Testing project
    Let's hope this works

--- MemoryGame.Test/Properties/AssemblyInfo.cs
+++ MemoryGame.Test/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("MemoryGame.Test2")]
+[assembly: AssemblyTitle("MemoryGame.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MemoryGame.Test2")]
+[assembly: AssemblyProduct("MemoryGame.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("e90dc894-c139-4e8f-ac79-c84ca1c538cc")]
+[assembly: Guid("6a97bcd0-756d-4c8f-b061-e9c84701f437")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
